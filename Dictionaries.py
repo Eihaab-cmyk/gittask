1e54b49c5f4e18edae9cf5f7c816f3585d8d9926
Eihaab-cmyk <muhammadeihaab5@gmail.com>
2025-10-28 17:59:19 +0500

    Short project

--- Dictionaries.py
+++
@@ -1,7 +0,0 @@
-student = {}
-
-student["name"] = input("Enter name of the student: ")
-student["age"] = int(input("Enter age of student: "))
-student["grade"] = input("Enter student grade: ")
-
-print(student)
